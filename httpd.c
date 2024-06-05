2e5a5df2d6fdeb2dd0266688817f230101fcc80f
Matthew <matthewf725@gmail.com>
2024-06-05 12:00:59 -0700

    created server socket, client socket, binded server socket, and working on thread creation
    Signed-off-by: Matthew <matthewf725@gmail.com>

--- httpd.c
+++ httpd.c
@@ -3,6 +3,15 @@
 int main(int argc, char *argv[]) {
     if(argc != 2){
         printf("ERROR: Must input TCP Port");
+        return 1;
     }
+
+    int portNum = argv[1];
+    if(portNum < 1024 || portNum > 65535){
+        printf("ERROR: Port number must be between 1024 and 65535");
+        return 1;
+    }
+
+
     return 0;
 }
--- httpd.h
+++ httpd.h
@@ -1,1 +1,4 @@
-#include <stdio.h>>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>

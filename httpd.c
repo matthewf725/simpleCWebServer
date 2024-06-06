ad7342c19beb9c10629dd49c5677500235dcebe7
Matthew <matthewf725@gmail.com>
2024-06-05 20:56:35 -0700

    fixed issue of starting to print empty body of header st.size times by making the input body null instead of empty, also fixed method names in handleRequest function
    Signed-off-by: Matthew <matthewf725@gmail.com>

--- httpd.c
+++ httpd.c
@@ -18,7 +18,7 @@ void send_response(int socket, const char *status, const char *content_type, con
     }
 }
 
-void handle_get_or_send_request(int socket, char* path, int get){
+void handle_get_or_head_request(int socket, char* path, int get){
     if (strstr(path, "..") != NULL || strchr(path, '~') != NULL) {
         char errMsg[] = "HTTP/1.1 401 Unauthorized  (Filepath contains '..' or '~')\r\n";
         send_response(socket, "401 Bad Request", "text/html", errMsg, strlen(errMsg));
@@ -37,7 +37,7 @@ void handle_get_or_send_request(int socket, char* path, int get){
                 send_response(socket, "200 OK", "text/html", file_contents, st.st_size + 1);
             } else {
                 fclose(fp);
-                send_response(socket, "200 OK", "text/html", "", st.st_size + 1);
+                send_response(socket, "200 OK", "text/html", NULL, st.st_size + 1);
             }
         }
     }
@@ -58,9 +58,9 @@ void *handleRequest(void *arg) {
             char errMsg[] = "HTTP/1.1 400 Bad Request  (malformed or unparseable HTTP request)\r\n";
             send_response(newsock, "400 Bad Request", "text/html", errMsg, strlen(errMsg));
         } else if (strcmp(method, "GET") == 0) {
-            handle_get_request(newsock, path, 1);
+            handle_get_or_head_request(newsock, path, 1);
         } else if (strcmp(method, "HEAD") == 0) {
-            handle_head_request(newsock, path, 0);
+            handle_get_or_head_request(newsock, path, 0);
         } else {
             char errMsg[] = "HTTP/1.1 501 Not Implemented  (request that uses an action other than HEAD or GET)\r\n";
             send_response(newsock, "501 Not Implemented", "text/html", errMsg, strlen(errMsg));
